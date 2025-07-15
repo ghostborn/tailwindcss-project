03f74b6edc0f888f0bdc75108c53f3d6618aa6b2
贾丽鹏 <1094425279@qq.com>
2025-07-15 15:32:13 +0800

    code commit

--- src/components/Blog1.vue
+++ src/components/Blog1.vue
@@ -1,13 +1,8 @@
-<script setup lang='ts'>
-import { ref, reactive } from 'vue'
-
-</script>
-
 <template>
   <section class="text-gray-600 body-font">
     <div class="container px-5 py-24 mx-auto">
       <div class="flex flex-wrap -m-4">
-        <div class="p-4 ">
+        <div class="p-4 md:w-1/3">
           <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
             <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog">
             <div class="p-6">
@@ -37,7 +32,7 @@ import { ref, reactive } from 'vue'
             </div>
           </div>
         </div>
-        <div class="p-4 ">
+        <div class="p-4 md:w-1/3">
           <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
             <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/721x401" alt="blog">
             <div class="p-6">
@@ -66,7 +61,7 @@ import { ref, reactive } from 'vue'
             </div>
           </div>
         </div>
-        <div class="p-4 ">
+        <div class="p-4 md:w-1/3">
           <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
             <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402" alt="blog">
             <div class="p-6">
@@ -101,6 +96,8 @@ import { ref, reactive } from 'vue'
   </section>
 </template>
 
-<style scoped>
+<script setup lang='ts'>
+</script>
 
+<style scoped>
 </style>
